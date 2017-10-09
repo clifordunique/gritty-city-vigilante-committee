51c134cd2e72612f5cb7f162b37988c01bd84b9d
Hendricks <dubmore@gmail.com>
2017-10-08 20:35:44 -1000

    mvp started
    basic player and enemy

---
+++ hdrx-mvp/Assets/CameraSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour {
+
+    private GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public fl
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
