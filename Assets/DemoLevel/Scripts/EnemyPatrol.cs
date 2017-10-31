543b14d08e3ad8c9f1c5426f9f07ae311326c97f
Aditya Kumar <adityak@hawaii.edu>
2017-10-30 18:49:55 -1000

    Enemy Now stops moving and to attack enemy (doesnt wait though so continually attacks)

--- Assets/DemoLevel/Scripts/EnemyPatrol.cs
+++ Assets/DemoLevel/Scripts/EnemyPatrol.cs
@@ -13,6 +13,7 @@ public class EnemyPatrol : PhysicsObject {
 	Rigidbody2D myBody; 
 	Transform myTrans; 
 	float myWidth;
+	public bool attacking; 
 
 
 	public Collider[] attackHitboxes; // Assigned in Inspector with size of 4, to accomodate left/right arm, and left/right leg
@@ -24,45 +25,46 @@ public class EnemyPatrol : PhysicsObject {
 		myTrans = this.transform;
 		myBody = this.GetComponent<Rigidbody2D> (); 
 		myWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+		attacking = false;
 	}
 
 	// Vaguely using tutorial found https://www.youtube.com/watch?v=LPNSh9mwT4w
 	// Takes the place of Input.GetAxis("Horizontal"), and returns a float < 1 and > -1 
 	private float aiHorizontal()
 	{
-		//Checks if left side of the enemy is grounded 
-		Vector2 lineCastPosLeft = myTrans.position - myTrans.right * myWidth;
-		// Debug.DrawLine (lineCastPosLeft, lineCastPosLeft + Vector2.down);
-		bool willBeGroundedLeft = Physics2D.Linecast (lineCastPosLeft, lineCastPosLeft + Vector2.down, enemyMask);
+		if (attacking == false) {
+			//Checks if left side of the enemy is grounded 
+			Vector2 lineCastPosLeft = myTrans.position - myTrans.right * myWidth;
+			// Debug.DrawLine (lineCastPosLeft, lineCastPosLeft + Vector2.down);
+			bool willBeGroundedLeft = Physics2D.Linecast (lineCastPosLeft, lineCastPosLeft + Vector2.down, enemyMask);
 
-		//Checks if left side of the enemy is grounded 
-		Vector2 lineCastPosRight = myTrans.position + myTrans.right * myWidth;
-		// Debug.DrawLine (lineCastPosRight, lineCastPosRight + Vector2.down);
-		bool willBeGroundedRight = Physics2D.Linecast (lineCastPosRight, lineCastPosRight + Vector2.down, enemyMask);
+			//Checks if left side of the enemy is grounded 
+			Vector2 lineCastPosRight = myTrans.position + myTrans.right * myWidth;
+			// Debug.DrawLine (lineCastPosRight, lineCastPosRight + Vector2.down);
+			bool willBeGroundedRight = Physics2D.Linecast (lineCastPosRight, lineCastPosRight + Vector2.down, enemyMask);
 
-		if (facingRight == true) //If the enemy is currently facing right continue moving right unless it will not be grounded
-		{ 
-			// Debug.Log ("entering right case");
-			if (!willBeGroundedRight) 
-			{
-				// Debug.Log ("Will not be grounded on right");
-				facingRight = false;//Face the left
-				return 0f; 
-			} else 
-			{
-				return 0.1f;
+			if (facingRight == true) { //If the enemy is currently facing right continue moving right unless it will not be grounded
+				// Debug.Log ("entering right case");
+				if (!willBeGroundedRight) {
+					// Debug.Log ("Will not be grounded on right");
+					facingRight = false;//Face the left
+					return 0f; 
+				} else {
+					return 0.1f;
+				}
+			} else { //If the enemy is currently facing left continue moving left unless it will not be grounded
+				// Debug.Log ("entering left case");
+				if (!willBeGroundedLeft) {
+					// Debug.Log ("Will not be grounded on left");
+					facingRight = true;//Face the left
+					return 0; 
+				} else {
+					return -0.1f;
+				}
 			}
-		} else //If the enemy is currently facing left continue moving left unless it will not be grounded
+		} else 
 		{
-			// Debug.Log ("entering left case");
-			if (!willBeGroundedLeft) {
-				// Debug.Log ("Will not be grounded on left");
-				facingRight = true;//Face the left
-				return 0; 
-			} else 
-			{
-				return -0.1f;
-			}
+			return 0;
 		}
 	}
 
@@ -70,23 +72,20 @@ public class EnemyPatrol : PhysicsObject {
 	{
 		Vector2 move = Vector2.zero;
 		move.x = aiHorizontal ();
-
 		//makes sprite flip back and forth depending on direction moving
 		bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
-
 		if (flipSprite)
 		{
 			spriteRenderer.flipX = !spriteRenderer.flipX;
 		}
-
 		animator.SetBool("grounded", grounded);
 		animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 		targetVelocity = move * maxSpeed;
 	}
-
-
+		
 	private bool aiAttack()
 	{
+		
 		Debug.Log ("Running aiAttack");
 		foreach (Collider col in attackHitboxes) 
 		{
@@ -99,6 +98,7 @@ public class EnemyPatrol : PhysicsObject {
 		Debug.Log ("Didn't find anything to attack");
 		return false; 
 	}
+
 	// Checks for keypress and calls launch attack with the corresponding attackHitBox 
 	// Needs to be called in update
 	// Following tutorial found at https://www.youtube.com/watch?v=mvVM1RB4HXk 
@@ -106,6 +106,7 @@ public class EnemyPatrol : PhysicsObject {
 	{
 		if (aiAttack()) 
 		{
+			attacking = true;
 			if (spriteRenderer.flipX) //Checks the direction the sprite is facing and selects the correct arm side
 			{
 				Debug.Log ("left enemy arm attack");
@@ -136,6 +137,7 @@ public class EnemyPatrol : PhysicsObject {
 	// Following tutorial found at https://www.youtube.com/watch?v=mvVM1RB4HXk 
 	private void LaunchAttack(Collider col)
 	{
+		animator.SetBool("attacking", attacking);
 		//		Debug.Log ("Launching attack"); 
 		Collider[] cols = Physics.OverlapBox(col.bounds.center,col.bounds.extents,col.transform.rotation, LayerMask.GetMask("Hitbox")); 
 		foreach(Collider c in cols)
