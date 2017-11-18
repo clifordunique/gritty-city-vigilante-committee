fec86e4398b0d73fb3ded0181e1463a138a98d73
Aditya Kumar <adityak@hawaii.edu>
2017-11-18 13:30:55 -1000

    A Enemy Spawner Script to spawn enemies from a point
    Must be linked to a game object

---
+++ Alan Prototype cause fuck unity/fuck Unity/Assets/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject[] enemyPlayer; 
+	public Vector3 spawnValues; 
+	private float spawnWait; 
+	public float spawnMostWait;
+	public float spawnLeastWait; 
+	public int startWait; 
+	public bool stop; 
+	private Quaternion originalRot = Quaternion.Euler(0, 0, 0);
+	private Transform myTransform;
+
+
+	void Start () 
+	{
+		myTransform = this.transform;
+		StartCoroutine (waitSpawner()); 
+	}
+
+	void Update () 
+	{
+		spawnWait = Random.Range (spawnLeastWait, spawnMostWait); 
+	}
+
+	IEnumerator waitSpawner()
+	{
+		yield return new WaitForSeconds (startWait); 
+
+		while (!stop) 
+		{
+			Vector2 spawnPosition = new Vector2(myTransform.position.x, myTransform.position.y);
+			Debug.Log ("x " + myTransform.position.x + " y " + myTransform.position.y); 
+
+			Instantiate (enemyPlayer[0], spawnPosition, originalRot); 
+
+			yield return new WaitForSeconds (spawnWait); 
+		}
+	}
+}
