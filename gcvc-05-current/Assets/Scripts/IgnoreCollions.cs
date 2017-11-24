18488ebaca73af5a0f8233e3da8d3684aa206bf4
Hendricks <dubmore@gmail.com>
2017-11-24 12:39:36 -1000

    sperated repos

---
+++ gcvc-05-current/Assets/Scripts/IgnoreCollions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollions : MonoBehaviour {
+
+    private Collider2D _hurtBox;
+    public bool test = true;
+	// Use this for initialization
+	void Start () {
+        _hurtBox = GetComponent<BoxCollider2D>(); ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D enemy)
+    {
+
+        if (enemy.gameObject.tag == "enemy")
+        {
+            Debug.Log("enemy collision");
+            Physics2D.IgnoreCollision(enemy, _hurtBox, test);
+        }
+    }
+}
+
+
---
+++ gcvc-05-current/Assets/triggerMan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerMan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit");
+        // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+        if (other.gameObject.tag == "mothaFuckin_shot")
+        {
+            Debug.Log("do it");
+            // Make the other game object (the pick up) inactive, to make it disappear
+            //other.gameObject.SetActive(false);
+            Destroy(gameObject, .5f);
+            // Add one to the score variable 'count'
+            //count = count + 1;
+
+            // Run the 'SetCountText()' function (see below)
+            //SetCountText();
+        }
+    }
+}
+
